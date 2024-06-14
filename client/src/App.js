f40a4706f4820d7895ecab565e4714d695931f6a
Emaan Baluj <emaanbaluj@icloud.com>
2024-06-14 12:54:25 +0100

    Added Figma Design File

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="Logo" className="App-logo" />
         <a className="App-link" href="http://localhost:5000/">Login with Spotify</a>
+        <div className="Y letter">Y</div> {/* Display the letter "Y" */}
+        <div className="O1 letter">O</div> {/* Display the letter "O" */}
+        <div className="O2 letter">O</div> {/* Display the letter "O" */}
+        <div className="Z letter">Z</div> {/* Display the letter "Z" */}
       </header>
       <div className="Image-container">
         <img src={img1} alt="Collage 1" className="App-image" />
