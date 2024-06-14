3edd86d3ddf6735a27633a847e01aa31011a7ff7
Emaan Baluj <emaanbaluj@icloud.com>
2024-06-14 12:06:03 +0100

    Created Background with Centred Logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import img1 from './images/collage1.jpg'; 
+import logo from './images/yoozlogo.png'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="Logo" className="App-logo" />
+        <a className="App-link" href="http://localhost:5000/">Login with Spotify</a>
       </header>
+      <div className="Image-container">
+        <img src={img1} alt="Collage 1" className="App-image" />
+      </div>
     </div>
   );
 }
