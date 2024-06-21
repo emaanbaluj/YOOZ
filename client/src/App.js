dfc3531f55e42027b0ab12b067ad2eef5a0ea7b9
Emaan Baluj <emaanbaluj@icloud.com>
2024-06-21 20:53:02 +0100

    Added 'Connect With' with one horizonatal line on left and right

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="Y letter">Y</div> {/* Display the letter "Y" */}
-        <div className="O1 letter">O</div> {/* Display the letter "O" */}
-        <div className="O2 letter">O</div> {/* Display the letter "O" */}
-        <div className="Z letter">Z</div> {/* Display the letter "Z" */}
-        <div className="Header1">Hear More,</div> 
+      <header className="App-header" style={{ overflow: 'visible' }}>
+        <div className="Y letter">Y</div>
+        <div className="O1 letter">O</div>
+        <div className="O2 letter">O</div>
+        <div className="Z letter">Z</div>
+        <div className="Header1">Hear More,</div>
         <div className="Header2">Know More, With</div>
+        <div className="ConnectWithContainer">
+          <hr className="leftLine" />
+          <div className="ConnectWith">Connect With</div>
+          <hr className="rightLine" />
+        </div>
       </header>
     </div>
   );
